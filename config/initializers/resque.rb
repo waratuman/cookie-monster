6f99b049252e2161a186728048d75c03c3d00cb2
waratuman <waratuman@gmail.com>
2010-07-26 22:53:03 -0700

    This should only be set if the ENV var is not set

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
-ENV["REDISTOGO_URL"] = "redis://waratuman:a93393efc068c3e08bd48720f2da1a00@goosefish.redistogo.com:9040/"
+ENV["REDISTOGO_URL"] ||= "redis://waratuman:a93393efc068c3e08bd48720f2da1a00@goosefish.redistogo.com:9040/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
